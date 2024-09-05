d9a8ae16f4ecde0972c6cdfda00ea2ffe5d49edf
walid cadi <cadiwalid02@gmail.com>
2024-09-05 12:12:35 +0100

    resolve img  problem

--- src/constant/images.jsx
+++ src/constant/images.jsx
@@ -1,6 +1,8 @@
 let dataImages = {
     login: require("../assets/images/login-bg.jpg"),
    google: require("../assets/images/download.jpeg"),
-   facebbok: require("../assets/images/facebook.jpeg")
+    facebbok: require("../assets/images/facebook.jpeg"),
+    profil: require("../assets/images/profile.jpg"),
 }
 export default dataImages
+
