9ad8e3dc106508cf4b19ef2f8349e6c7642cb91f
mortyrex34 <achraferrabbaa1@gmail.com>
2024-09-09 16:52:09 +0100

    add market

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { MyProvider } from "./context";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./index.css";
+import Market from "./pages/market/market";
+import Marketinfo from "./pages/market/marketInfo";
 
 
 function App() {
   return (
     <>
+    <Routes>
+        <Route path="/" element={<Market />} />
+        <Route path="/marketinfo/:id" element={<Marketinfo />} />
+      </Routes>
+    
+    
   <MyProvider>
     
   </MyProvider>
