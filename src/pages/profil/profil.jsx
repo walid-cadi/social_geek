248f2b1b63f28a8af7f947fad9eed5595c8af202
walid cadi <cadiwalid02@gmail.com>
2024-09-10 10:41:35 +0100

    merge

--- src/pages/profil/profil.jsx
+++ src/pages/profil/profil.jsx
@@ -3,11 +3,10 @@ import { Post } from "../components/post";
 import { MyAppContext } from "../../context";
 
 const Profil = () => {
-  const { userData } = MyAppContext(); 
+  const { userData } = MyAppContext();
 
-  
   if (!userData) {
-    return <p>Loading...</p>; 
+    return <p>Loading...</p>;
   }
 
   return (
@@ -17,29 +16,35 @@ const Profil = () => {
           <div className="rounded-t-lg h-32 overflow-hidden">
             <img
               className="object-cover object-top w-full"
-              src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+              src={userData.cover}
               alt="Mountain"
             />
           </div>
           <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
             <img
               className="object-cover object-center h-32"
-              src={userData.profile || 'default-profile-image-url'} // Use a default image if userData.profile is not available
+              src={userData.profile || "default-profile-image-url"} // Use a default image if userData.profile is not available
               alt="Profile"
             />
           </div>
           <div className="text-center mt-2">
-            <h2 className="font-semibold">{userData.firstName || 'First Name'}</h2>
-            <p className="text-gray-500">{userData.email || 'Email'}</p>
+            <h2 className="font-semibold">
+              {userData.firstName || "First Name"}
+            </h2>
+            <p className="text-gray-500">{userData.email || "Email"}</p>
           </div>
           <ul className="flex justify-center pt-3 gap-7">
             <li className="flex flex-col items-center justify-around">
               <h3 className="text-base font-bold">Posts</h3>
-              <h6 className="text-base font-bold">{userData.post?.length || 0}</h6>
+              <h6 className="text-base font-bold">
+                {userData.post?.length || 0}
+              </h6>
             </li>
             <li className="flex flex-col items-center justify-around">
               <h3 className="text-base font-bold">Followers</h3>
-              <h6 className="text-base font-bold">{userData.followers.length}</h6>
+              <h6 className="text-base font-bold">
+                {userData.followers.length}
+              </h6>
             </li>
             <li className="flex flex-col items-center justify-around">
               <h3 className="text-base font-bold">Following</h3>
@@ -52,7 +57,8 @@ const Profil = () => {
             <div className="p-4">
               <h4 className="font-bold mb-3 text-sm text-gray-900">Bio</h4>
               <p className="font-medium text-gray-500 leading-6 text-xs mb-0">
-                {userData.bio || "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus."}
+                {userData.bio ||
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus."}
               </p>
             </div>
             <div className="border-t flex items-center p-4">
